6227ad9844264332840dcf2d1c3357a4c6460781
jivory <ivory.justin@gmail.com>
2015-07-03 14:36:17 -0400

    setting up sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,21 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.action_mailer.default_url_options = { :host => 'jivory1.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'jivory1.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
